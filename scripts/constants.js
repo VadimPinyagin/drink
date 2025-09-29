d87d1de68eb3541c6997a9bd2ef2f220686e1618
VadimPinyagin <vadimbox85@gmail.com>
2025-09-29 03:39:44 +0000

    Deploying to gh-pages from @ VadimPinyagin/drink@01757202a0bf570e41b649ddc7abd8c9b611a237 🚀

---
+++ scripts/constants.js
@@ -0,0 +1,1 @@
+var e=document.querySelector(".filter-price__range");export{e as sliderElement};
---
+++ scripts/index.js
@@ -0,0 +1,1 @@
+var e=document.querySelector(".filter-price__range");noUiSlider.create(e,{start:[0,900],connect:[!1,!0,!1],range:{min:0,max:1e3}});
