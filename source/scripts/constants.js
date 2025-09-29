01757202a0bf570e41b649ddc7abd8c9b611a237
VadimPinyagin <vadimbox85@gmail.com>
2025-09-29 06:39:07 +0300

    Добавляет слайдер диапазона цены

---
+++ source/scripts/constants.js
@@ -0,0 +1,1 @@
+export const sliderElement = document.querySelector('.filter-price__range');
--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,2 +1,11 @@
 /* в этот файл добавляет скрипты*/
+import {sliderElement} from './constants.js';
 
+noUiSlider.create(sliderElement, {
+  start: [0,900],
+  connect: [false, true, false],
+  range: {
+    min: 0,
+    max: 1000,
+  },
+});
