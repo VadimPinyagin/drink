212c7929dcc326cc12a7aca8983b761ced858249
Vadim <vadimbox85@gmail.com>
2025-09-29 20:27:29 +0300

    Работа с переключателем.

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -2,3 +2,9 @@
 import rangeSlider from './range-slider.js';
 
 rangeSlider();
+
+function toggleCheckbox(checkboxId) {
+    const checkbox = document.getElementById('#brazil');
+    checkbox.checked = !checkbox.checked; // Переключаем состояние чекбокса
+}
+toggleCheckbox();
