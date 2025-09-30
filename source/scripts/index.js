cc511ed12747333d8d25b1eca6a81c26281c429b
VadimPinyagin <vadimbox85@gmail.com>
2025-09-30 03:11:58 +0300

    Добавляет переключатели для стран.

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,10 +1,4 @@
 /* в этот файл добавляет скрипты*/
-import rangeSlider from './range-slider.js';
+import {rangeSlider} from './range-slider.js';
 
 rangeSlider();
-
-function toggleCheckbox(checkboxId) {
-    const checkbox = document.getElementById('#brazil');
-    checkbox.checked = !checkbox.checked; // Переключаем состояние чекбокса
-}
-toggleCheckbox();
